5e1a845d3b66b3c28de8c5eaa9694365bd8c7b69
way2atit <way2atit@gmail.com>
2023-06-05 20:16:05 +0530

    test2

--- src/test/java/com/gitcode/Test2.java
+++ src/test/java/com/gitcode/Test2.java
@@ -2,6 +2,42 @@ package com.gitcode;
 
 public class Test2 {
 
+	//  
+	// 
+	public static void name() {
+		
+		System.out.println("Hello");
+		
+	}
+	
+	
+	
+	
 }
-
+// pull
 // workspace  --  
+
+// Hello
+// Test2
+
+
+// git init
+// git status
+// git add .
+// git commit -m "msg"
+
+// git remote add origin "url"
+// git config --global user.name "name"
+// git config --global user.email  "email"
+
+// git push origin master
+// git pull origin master
+
+
+//  git branch branch_name
+//  git branch
+//  git checkout branch_name
+
+
+
+
